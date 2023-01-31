9ea56705a0b186ef37b13d0e5f1f44b8ee623da5
Maria T <tanachakova.maria@yandex.ru>
2023-01-31 23:49:13 +0700

    refactor: добавить dao-классы, изменить на mapstruct

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.service;
 
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.UserNotFoundException;
 
 import java.util.Collection;
