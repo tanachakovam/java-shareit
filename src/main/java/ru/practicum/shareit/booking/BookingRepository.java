2c89a8845052340ead9bc14cb6a1f9280420438d
Maria T <tanachakova.maria@yandex.ru>
2023-02-14 18:44:15 +0700

    14th sprint

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -7,7 +7,6 @@ import ru.practicum.shareit.booking.model.BookingStatus;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
